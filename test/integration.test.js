4b337e068310708e29de2162dcf0972ad75cd599
Luis Gomez <lgomez@gmail.com>
2015-04-16 01:03:15 -0700

    Cleaning up nesting.

--- test/integration.test.js
+++ test/integration.test.js
@@ -24,7 +24,7 @@ servers.forEach(function(server){
 
     server.parsed = url.parse(server.config.url);
 
-    describe('Testing: ' + server.parsed.host, function(){
+    describe('Integration against: ' + server.parsed.host, function(){
 
         var instance = new RETS(server.config);
         var fixture = path.join(__dirname, 'fixtures') + '/' + server.parsed.host + '.json';
@@ -68,57 +68,50 @@ servers.forEach(function(server){
 
 
         if (typeof server.metadata !== 'undefined') {
-            describe.skip('Metadata', function(){
-                var metadata = '';
-                this.timeout(30000);
-                it('Can get resources',function(done){
-                    instance.on('medata', done);
-                    instance.getMetadata({ Type:'METADATA-RESOURCE', ID: server.metadata.resources.ID }).on('data',function(line){
-                        metadata += line.toString();
-                    });
+            var metadata = '';
+            it.skip('Can get resources',function(done){
+                instance.on('medata', done);
+                instance.getMetadata({ Type:'METADATA-RESOURCE', ID: server.metadata.resources.ID }).on('data',function(line){
+                    metadata += line.toString();
                 });
-                it('Can get classes',function(done){
-                    instance.on('medata', done);
-                    instance.getMetadata({ Type:'METADATA-CLASS', ID: server.metadata.classes.ID }).on('data',function(line){
-                        metadata += line.toString();
-                    });
-                });
-
-                it('Can get fields',function(done){
-                    instance.on('medata', done);
-                    instance.getMetadata({ Type:'METADATA-TABLE', ID: server.metadata.fields.ID }).on('data',function(line){
-                        metadata += line.toString();
-                    });
-                });
-
-                it('Can get status values',function(done){
-                    instance.on('medata', done);
-                    instance.getMetadata({ Type:'METADATA-LOOKUP_TYPE', ID: server.metadata.status.ID }).on('data',function(line){
-                        metadata += line.toString();
-                    });
-                });
-
-                it('Can get metadata as a native object');
-                it('Can stream metadata to an xml file');
-                it('Can search for property listings');
-                it('Can send raw stream to an xml file');
-                it('Can send a csv stream to a csv file');
-                it('Can get stream of objects');
-                it('Can get stream of arrays');
-                it('Can get object from the server: NOT IMPLEMENTED');
-
             });
+            it.skip('Can get classes',function(done){
+                instance.on('medata', done);
+                instance.getMetadata({ Type:'METADATA-CLASS', ID: server.metadata.classes.ID }).on('data',function(line){
+                    metadata += line.toString();
+                });
+            });
+
+            it.skip('Can get fields',function(done){
+                instance.on('medata', done);
+                instance.getMetadata({ Type:'METADATA-TABLE', ID: server.metadata.fields.ID }).on('data',function(line){
+                    metadata += line.toString();
+                });
+            });
+
+            it.skip('Can get status values',function(done){
+                instance.on('medata', done);
+                instance.getMetadata({ Type:'METADATA-LOOKUP_TYPE', ID: server.metadata.status.ID }).on('data',function(line){
+                    metadata += line.toString();
+                });
+            });
+
+            it('Can get metadata as a native object');
+            it('Can stream metadata to an xml file');
+            it('Can search for property listings');
+            it('Can send raw stream to an xml file');
+            it('Can send a csv stream to a csv file');
+            it('Can get stream of objects');
+            it('Can get stream of arrays');
+            it('Can get object from the server: NOT IMPLEMENTED');
         }
 
         if (server.search) {
-            describe.skip('Search', function(){
-                var search = '';
-                this.timeout(30000);
-                it('Can search for property listings', function(done){
-                    instance.on('search', done);
-                    instance.search(server.search).on('data',function(line){
-                        search += line.toString();
-                    });
+            var search = '';
+            it.skip('Can search for property listings', function(done){
+                instance.on('search', done);
+                instance.search(server.search).on('data',function(line){
+                    search += line.toString();
                 });
             });
         }
