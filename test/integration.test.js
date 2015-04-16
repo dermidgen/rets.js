4852daa142da9911ad8d98cf4edb6f266ee4ff53
Luis Gomez <lgomez@gmail.com>
2015-04-16 14:29:12 -0700

    Moving config for localhost server up a level and loading accordingly. It is not meant to be edited by users anymore as we’ll provide a different way to load configs for live servers. This is only to cover tests for mocked localhost.

--- test/integration.test.js
+++ test/integration.test.js
@@ -18,108 +18,64 @@ if (record) {
     });
 }
 
-var servers = require('./config.json');
+var config_path = process.env.CONFIG_PATH || path.join(__dirname, './config.json');
 
-servers.forEach(function(server){
-
+var server = require(config_path);
     server.parsed = url.parse(server.config.url);
+var instance = new RETS(server.config);
 
-    describe('Integration against: ' + server.parsed.host, function(){
-
-        var instance = new RETS(server.config);
         var fixture = path.join(__dirname, 'fixtures') + '/' + server.parsed.host + '.json';
-        this.timeout(30000);
+describe('Integration against: ' + instance.session.url.host, function(){
 
-        before("Server Tests Setup", function(done){
-            if (record && fs.existsSync(fixture) && server.parsed.host !== 'localhost') {
-                fs.unlink(fixture, function(){
-                    done();
-                });
-            } else {
-                if (fs.existsSync(fixture)) {
-                    nock.load(fixture);
-                }
-                done();
-            }
+    this.timeout(30000);
 
+    it('Can login', function(done){
+        instance.on('login', done).login(function(/*err, response, body*/){
+            // console.log(body);
         });
+    });
 
-        after("Server Test Cleanup", function(done){
-            if (record && server.parsed.host !== 'localhost') {
-                fs.writeFile(fixture, JSON.stringify(nock.recorder.play(), null, 4), function(){
-                    done();
-                });
-            } else {
-                done();
-            }
-        });
-
-        it('Can login', function(done){
-
-            instance.on('login', done).login(function(err, response, body){
-                console.log(body);
-            });
-        });
-
-        it.skip('Can read capabilities from the server', function(){
-            console.log(instance.session.capabilities);
-            // assert(instance.session.capabilities.Search && instance.session.capabilities.GetMetadata);
-        });
+    it.skip('Can read capabilities from the server', function(){
+        // console.log(instance.session.capabilities);
+        // assert(instance.session.capabilities.Search && instance.session.capabilities.GetMetadata);
+    });
 
 
-        if (typeof server.metadata !== 'undefined') {
-            var metadata = '';
-            it.skip('Can get resources',function(done){
-                instance.on('medata', done);
-                instance.getMetadata({ Type:'METADATA-RESOURCE', ID: server.metadata.resources.ID }).on('data',function(line){
-                    metadata += line.toString();
-                });
-            });
-            it.skip('Can get classes',function(done){
-                instance.on('medata', done);
-                instance.getMetadata({ Type:'METADATA-CLASS', ID: server.metadata.classes.ID }).on('data',function(line){
-                    metadata += line.toString();
-                });
-            });
+    it.skip('Can get resources',function(done){
+        instance.on('medata', done);
+        instance.getMetadata({ Type:'METADATA-RESOURCE', ID: server.metadata.resources.ID });
+    });
+    it.skip('Can get classes',function(done){
+        instance.on('medata', done);
+        instance.getMetadata({ Type:'METADATA-CLASS', ID: server.metadata.classes.ID });
+    });
 
-            it.skip('Can get fields',function(done){
-                instance.on('medata', done);
-                instance.getMetadata({ Type:'METADATA-TABLE', ID: server.metadata.fields.ID }).on('data',function(line){
-                    metadata += line.toString();
-                });
-            });
+    it.skip('Can get fields',function(done){
+        instance.on('medata', done);
+        instance.getMetadata({ Type:'METADATA-TABLE', ID: server.metadata.fields.ID });
+    });
 
-            it.skip('Can get status values',function(done){
-                instance.on('medata', done);
-                instance.getMetadata({ Type:'METADATA-LOOKUP_TYPE', ID: server.metadata.status.ID }).on('data',function(line){
-                    metadata += line.toString();
-                });
-            });
+    it.skip('Can get status values',function(done){
+        instance.on('medata', done);
+        instance.getMetadata({ Type:'METADATA-LOOKUP_TYPE', ID: server.metadata.status.ID });
+    });
 
-            it('Can get metadata as a native object');
-            it('Can stream metadata to an xml file');
-            it('Can search for property listings');
-            it('Can send raw stream to an xml file');
-            it('Can send a csv stream to a csv file');
-            it('Can get stream of objects');
-            it('Can get stream of arrays');
-            it('Can get object from the server: NOT IMPLEMENTED');
-        }
+    it('Can get metadata as a native object');
+    it('Can stream metadata to an xml file');
+    it('Can search for property listings');
+    it('Can send raw stream to an xml file');
+    it('Can send a csv stream to a csv file');
+    it('Can get stream of objects');
+    it('Can get stream of arrays');
+    it('Can get object from the server: NOT IMPLEMENTED');
 
-        if (server.search) {
-            var search = '';
-            it.skip('Can search for property listings', function(done){
-                instance.on('search', done);
-                instance.search(server.search).on('data',function(line){
-                    search += line.toString();
-                });
-            });
-        }
+    it.skip('Can search for property listings', function(done){
+        instance.on('search', done);
+        instance.search(server.search);
+    });
 
-        it.skip('Can logout', function(done){
-            instance.on('logout', done).logout();
-        });
-
+    it.skip('Can logout', function(done){
+        instance.on('logout', done).logout();
     });
 
 });
