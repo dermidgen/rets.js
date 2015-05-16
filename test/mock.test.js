3da6efb7c70148292e7a02a55ec65df3b8e30064
Luis Gomez <lgomez@gmail.com>
2015-05-15 19:36:43 -0700

    Activating and deactivating a number of tests

--- test/mock.test.js
+++ test/mock.test.js
@@ -1,6 +1,6 @@
-// var assert = require('assert');
-// var util = require('util');
-// var debug = require('debug')('rets.js:test:servers:mock');
+var assert = require('assert');
+var util = require('util');
+var debug = require('debug')('rets.js:test:servers:mock');
 // var fs = require('fs');
 var nock = require('nock');
 // var xml = require('xml2js').parseString;
@@ -35,173 +35,67 @@ describe('Mocked RETS Server calls',function(){
         rets = new RETS({url: "https://user:pass@example.com:9160/contact/rets/login"});
     });
 
-    it.only('Can login to a RETS server',function(done){
-
-        rets.on('login', function(){
-            done();
-        });
-
-        rets.on('error', function(err){
-            done(err);
-        });
-
+    it('Can login to a RETS server',function(done){
+        rets.on('login', done);
+        rets.on('error', done);
         rets.login();
-
     });
 
-    // it('Can read capabilities from the server',function(){
-    //     assert(rets.session.capabilities.Search && rets.session.capabilities.GetMetadata);
-    // });
+    it('Can read capabilities from the server', function(){
+        assert(rets.session.capabilities.Search && rets.session.capabilities.GetMetadata);
+    });
 
-    // it('Can get metadata as a native object',function(done){
-    //
-    //     loadFixture('metadata-resource');
-    //     rets.addListener('metadata',function(err, result){
-    //         rets.removeAllListeners('metadata');
-    //         assert(err === null);
-    //         assert(typeof result === 'object');
-    //     });
-    //
-    //     rets.getMetadata({ Type:'METADATA-RESOURCE', ID: '0'})
-    //     .on('finish', function(){
-    //         done();
-    //     });
-    // });
+    it('Can get metadata as a native object',function(done){
+        rets.on('metadata', function(err, result){
+            assert(err === null);
+            assert(typeof result === 'object');
+            done();
+        });
+        rets.getMetadata();
+    });
 
-    // it('Can stream metadata to an xml file',function(done){
-    //
-    //     loadFixture('metadata-resource');
-    //     rets.addListener('metadata',function(err, result){
-    //         rets.removeAllListeners('metadata');
-    //         assert(err === null);
-    //         assert(typeof result === 'object');
-    //
-    //         var results = getTestOutput('metadata.xml');
-    //         xml(results, function(parserr){
-    //             assert(parserr === null);
-    //             done();
-    //         });
-    //
-    //     });
-    //
-    //     rets.getMetadata({ Type:'METADATA-RESOURCE', ID: '0'})
-    //     .pipe(testStream('metadata.xml'))
-    //     .on('finish', function(){
-    //         // done(); we will wait until we have validated the final result
-    //     });
-    // });
+    it('Can search for property listings',function(done){
+        rets.on('search', function(err, res){
+            assert(err === null);
+            assert(res.count !== null);
+            assert(res.records !== null);
+            done();
+        });
+        rets.search({
+            SearchType: 'Property',
+            Class: 'Residential',
+            Query: '(TimestampModified=2015-04-01+),(Status=|A)',
+            QueryType: 'DMQL2',
+            Offset:1,
+            Limit: 3,
+            StandardNames: 1
+        });
+    });
 
-    // it('Can search for property listings',function(done){
-    //
-    //     loadFixture('search');
-    //     rets.addListener('search',function(err, res){
-    //         rets.removeAllListeners('search');
-    //         assert(err === null);
-    //         assert(res.count !== null);
-    //         assert(res.records !== null);
-    //     });
-    //
-    //     rets.search({
-    //         SearchType: 'Property',
-    //         Class: 'Residential',
-    //         Query: '(TimestampModified=2015-04-01+),(Status=|A)',
-    //         QueryType: 'DMQL2',
-    //         Offset:1,
-    //         Limit: 3,
-    //         StandardNames: 1
-    //     })
-    //     .on('finish', function(){
-    //         done();
-    //     });
-    // });
+    it('Can get stream of objects',function(done){
 
-    // it('Can send raw stream to an xml file',function(done){
-    //
-    //     loadFixture('search');
-    //     rets.addListener('search',function(err, res){
-    //         rets.removeAllListeners('search');
-    //         assert(err === null);
-    //         assert(res.count !== null);
-    //         assert(res.records !== null);
-    //
-    //         var results = getTestOutput('listings-raw.xml');
-    //         xml(results, function(parserr){
-    //             assert(parserr === null);
-    //             done();
-    //         });
-    //
-    //     });
-    //
-    //     rets.search({
-    //         SearchType: 'Property',
-    //         Class: 'Residential',
-    //         Query: '(TimestampModified=2015-04-01+),(Status=|A)',
-    //         QueryType: 'DMQL2',
-    //         Offset:1,
-    //         Limit: 3,
-    //         StandardNames: 1
-    //     })
-    //     .raw.pipe(testStream('listings-raw.xml'))
-    //     .on('finish', function(){
-    //         // done(); we will wait until we have validated the final result
-    //     });
-    // });
-
-    // it('Can send a csv stream to a csv file',function(done){
-    //
-    //     loadFixture('search');
-    //     rets.addListener('search',function(err, res){
-    //         rets.removeAllListeners('search');
-    //         assert(err === null);
-    //         assert(res.count !== null);
-    //         assert(res.records !== null);
-    //     });
-    //
-    //     rets.search({
-    //         SearchType: 'Property',
-    //         Class: 'Residential',
-    //         Query: '(TimestampModified=2015-04-01+),(Status=|A)',
-    //         QueryType: 'DMQL2',
-    //         Offset:1,
-    //         Limit: 3,
-    //         StandardNames: 1,
-    //         objectMode: false,
-    //         format: 'csv',
-    //     })
-    //     .pipe(testStream('listings.csv'))
-    //     .on('finish', function(){
-    //         done();
-    //     });
-    // });
-
-    // it('Can get stream of objects',function(done){
-    //
-    //     loadFixture('search');
-    //     rets.addListener('search',function(err, res){
-    //         rets.removeAllListeners('search');
-    //         assert(err === null);
-    //         assert(res.count !== null);
-    //         assert(res.records !== null);
-    //     });
-    //
-    //     rets.search({
-    //         SearchType: 'Property',
-    //         Class: 'Residential',
-    //         Query: '(TimestampModified=2015-04-01+),(Status=|A)',
-    //         QueryType: 'DMQL2',
-    //         Offset:1,
-    //         Limit: 3,
-    //         StandardNames: 1,
-    //         objectMode: true,
-    //         format: 'objects',
-    //     })
-    //     .on('data', function(record){
-    //         assert(typeof record === 'object');
-    //     })
-    //     .on('finish', function(){
-    //         done();
-    //     });
-    // });
+        rets.search({
+            SearchType: 'Property',
+            Class: 'Residential',
+            Query: '(TimestampModified=2015-04-01+),(Status=|A)',
+            QueryType: 'DMQL2',
+            Offset:1,
+            Limit: 3,
+            StandardNames: 1,
+            objectMode: true,
+            format: 'objects',
+        });
+        rets.on('data', function(record){
+            debug("record: \n%s", util.inspect(record, {colors:true}));
+            assert(typeof record === 'object');
+        });
+        rets.on('finish', function(err, res){
+            assert(err === null);
+            assert(res.count !== null);
+            assert(res.records !== null);
+            done();
+        });
+    });
 
     // it('Can get stream of arrays',function(done){
     //
